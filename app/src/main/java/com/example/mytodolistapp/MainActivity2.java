13362f0d72b2684df4fb9e1be836399aad193ddc
xinhe <u7518549@anu.edu.au>
2023-04-27 22:22:23 +1000

    Initial commit

---
+++ app/src/main/java/com/example/mytodolistapp/MainActivity2.java
@@ -0,0 +1,13 @@
+package com.example.mytodolistapp;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
